24f7ea129eb21837c752eb29e214b8ee8fc59af9
Anton <antonhalki@gmail.com>
2025-01-30 22:34:08 +0300

    Revert "Yrchik ban"
    This reverts commit eb6b1e3b210a6c7194b123e5d79d44293d925a8e.

--- src/main/java/site/andorvini/handlers/SlashCommandHandler.java
+++ src/main/java/site/andorvini/handlers/SlashCommandHandler.java
@@ -8,7 +8,6 @@ import org.javacord.api.interaction.SlashCommandInteraction;
 
 import site.andorvini.Main;
 import site.andorvini.commands.*;
-import site.andorvini.miscellaneous.MiscMethods;
 import site.andorvini.players.GreetingPlayer;
 import site.andorvini.players.Player;
 import site.andorvini.queue.Queue;
@@ -37,94 +36,90 @@ public class SlashCommandHandler {
             Optional<ServerVoiceChannel> optionalBotVoiceChannel = null;
             ServerVoiceChannel botVoiceChannel = null;
 
-            if (interaction.getUser().getName().equals("white_ydg")) {
-                MiscMethods.respondImmediatelyWithString(interaction, "Юрчик иди нахуй");
-            } else {
+            try {
+                interactionServer = slashCommandCreateEvent.getInteraction().getServer().get();
+                interactionServerId = interactionServer.getId();
+
+                optionalUserVoiceChannel = interaction.getUser().getConnectedVoiceChannel(interactionServer);
+                userVoiceChannel = optionalUserVoiceChannel.get();
+
+                optionalBotVoiceChannel = api.getYourself().getConnectedVoiceChannel(interactionServer);
+                botVoiceChannel = optionalBotVoiceChannel.get();
+            } catch (NoSuchElementException ignored) {}
+
+            if (!queues.containsKey(interactionServerId)) {
+                queues.put(interactionServerId, new site.andorvini.queue.Queue());
+            }
+
+            if (!greetingPlayers.containsKey(interactionServerId)) {
+                greetingPlayers.put(interactionServerId, new GreetingPlayer());
+            }
+
+            if (!players.containsKey(interactionServerId)) {
+                players.put(interactionServerId, new Player());
+            }
+
+            Queue currentQueue = queues.get(interactionServerId);
+            GreetingPlayer currentGreetingPlayer = greetingPlayers.get(interactionServerId);
+            Player currentPlayer = players.get(interactionServerId);
+
+            Main.addLastTextChannel(interactionServerId, interaction.getChannel().get());
+
+            if (!interaction.getChannel().get().getType().isServerChannelType()) {
+                interaction.createImmediateResponder()
+                        .setContent("Use this bot only on server!")
+                        .respond()
+                        .join();
+            } else if (fullCommandName.equals("phony")) {
+                Phony.phony(api, interaction, optionalUserVoiceChannel, currentPlayer, optionalBotVoiceChannel, interactionServer, userVoiceChannel, currentGreetingPlayer);
+            } else if (fullCommandName.equals("play")) {
+                Play.play(api, interaction, interactionServer, optionalUserVoiceChannel, currentQueue, optionalBotVoiceChannel, currentPlayer, userVoiceChannel);
+            } else if (fullCommandName.equals("loop")) {
+                Loop.loop(interaction, currentPlayer);
+            } else if (fullCommandName.equals("leave")) {
+                Leave.leave(api, interaction, interactionServer, optionalBotVoiceChannel, botVoiceChannel, interactionServerId, currentPlayer, currentQueue, players);
+            } else if (fullCommandName.equals("sseblo")) {
+                if (System.getenv("DP_SOSANIE_TTS_ENABLED").equals("true")){
+                    Sseblo.sseblo(api, interaction, interactionServer, currentPlayer, currentGreetingPlayer, userVoiceChannel);
+                }
+            } else if (fullCommandName.equals("clear")) {
+                Clear.clear(interaction);
+            } else if (fullCommandName.equals("pause")) {
+                Pause.pause(interaction, currentPlayer);
+            } else if (fullCommandName.equals("np")) {
+                NowPlaying.np(interaction, currentPlayer);
+            } else if (fullCommandName.equals("random")) {
+                Random.random(api, interaction, interactionServer, currentPlayer, currentGreetingPlayer, optionalBotVoiceChannel, optionalUserVoiceChannel);
+            } else if (fullCommandName.equals("ping")) {
+                respondImmediatelyWithString(interaction, "Pong!");
+            } else if (fullCommandName.equals("lyrics")) {
+                respondImmediatelyWithString(interaction, "Not implemented yet. (Because musixmatch shit)");
+            } else if (fullCommandName.equals("volume")) {
+                Volume.volume(api, interaction, currentPlayer, interactionServer, currentGreetingPlayer);
+            } else if (fullCommandName.equals("queue")) {
                 try {
-                    interactionServer = slashCommandCreateEvent.getInteraction().getServer().get();
-                    interactionServerId = interactionServer.getId();
-
-                    optionalUserVoiceChannel = interaction.getUser().getConnectedVoiceChannel(interactionServer);
-                    userVoiceChannel = optionalUserVoiceChannel.get();
-
-                    optionalBotVoiceChannel = api.getYourself().getConnectedVoiceChannel(interactionServer);
-                    botVoiceChannel = optionalBotVoiceChannel.get();
-                } catch (NoSuchElementException ignored) {}
-
-                if (!queues.containsKey(interactionServerId)) {
-                    queues.put(interactionServerId, new site.andorvini.queue.Queue());
+                    respondImmediatelyWithEmbed(interaction, currentQueue.getQueueEmbed());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-
-                if (!greetingPlayers.containsKey(interactionServerId)) {
-                    greetingPlayers.put(interactionServerId, new GreetingPlayer());
-                }
-
-                if (!players.containsKey(interactionServerId)) {
-                    players.put(interactionServerId, new Player());
-                }
-
-                Queue currentQueue = queues.get(interactionServerId);
-                GreetingPlayer currentGreetingPlayer = greetingPlayers.get(interactionServerId);
-                Player currentPlayer = players.get(interactionServerId);
-
-                Main.addLastTextChannel(interactionServerId, interaction.getChannel().get());
-
-                if (!interaction.getChannel().get().getType().isServerChannelType()) {
-                    interaction.createImmediateResponder()
-                            .setContent("Use this bot only on server!")
-                            .respond()
-                            .join();
-                } else if (fullCommandName.equals("phony")) {
-                    Phony.phony(api, interaction, optionalUserVoiceChannel, currentPlayer, optionalBotVoiceChannel, interactionServer, userVoiceChannel, currentGreetingPlayer);
-                } else if (fullCommandName.equals("play")) {
-                    Play.play(api, interaction, interactionServer, optionalUserVoiceChannel, currentQueue, optionalBotVoiceChannel, currentPlayer, userVoiceChannel);
-                } else if (fullCommandName.equals("loop")) {
-                    Loop.loop(interaction, currentPlayer);
-                } else if (fullCommandName.equals("leave")) {
-                    Leave.leave(api, interaction, interactionServer, optionalBotVoiceChannel, botVoiceChannel, interactionServerId, currentPlayer, currentQueue, players);
-                } else if (fullCommandName.equals("sseblo")) {
-                    if (System.getenv("DP_SOSANIE_TTS_ENABLED").equals("true")){
-                        Sseblo.sseblo(api, interaction, interactionServer, currentPlayer, currentGreetingPlayer, userVoiceChannel);
-                    }
-                } else if (fullCommandName.equals("clear")) {
-                    Clear.clear(interaction);
-                } else if (fullCommandName.equals("pause")) {
-                    Pause.pause(interaction, currentPlayer);
-                } else if (fullCommandName.equals("np")) {
-                    NowPlaying.np(interaction, currentPlayer);
-                } else if (fullCommandName.equals("random")) {
-                    Random.random(api, interaction, interactionServer, currentPlayer, currentGreetingPlayer, optionalBotVoiceChannel, optionalUserVoiceChannel);
-                } else if (fullCommandName.equals("ping")) {
-                    respondImmediatelyWithString(interaction, "Pong!");
-                } else if (fullCommandName.equals("lyrics")) {
-                    respondImmediatelyWithString(interaction, "Not implemented yet. (Because musixmatch shit)");
-                } else if (fullCommandName.equals("volume")) {
-                    Volume.volume(api, interaction, currentPlayer, interactionServer, currentGreetingPlayer);
-                } else if (fullCommandName.equals("queue")) {
-                    try {
-                        respondImmediatelyWithEmbed(interaction, currentQueue.getQueueEmbed());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                } else if (fullCommandName.equals("skip")) {
-                    Skip.skip(api, interaction, interactionServer, optionalBotVoiceChannel, currentQueue, currentPlayer);
-                } else if (fullCommandName.equals("seek")) {
-                    Seek.seek(interaction, currentPlayer);
-                } else if (fullCommandName.equals("dev")) {
-                    DevCommand.triggerDevCommand(interaction, api);
-                } else if (fullCommandName.equals("change")) {
-                    ChangeBatteries.change(api, interactionServer);
-                } else if (fullCommandName.equals("randomprompt")){
-                    RandomPrompt.randomPrompt(interaction);
-                } else if (fullCommandName.equals("reklama")) {
-                    ReklamaCommand.reklama(api, interaction);
-                } else if (fullCommandName.equals("addgreeting")) {
-                    AddGreetingCommand.greetingCommand(interaction);
-                } else if (fullCommandName.equals("greetingdel")) {
-                    RemoveGreetingCommand.removeGreeting(interaction);
-                } else if (fullCommandName.equals("greetingls")) {
-                    GreetingListCommand.greetingList(interaction);
-                }
+            } else if (fullCommandName.equals("skip")) {
+                Skip.skip(api, interaction, interactionServer, optionalBotVoiceChannel, currentQueue, currentPlayer);
+            } else if (fullCommandName.equals("seek")) {
+                Seek.seek(interaction, currentPlayer);
+            } else if (fullCommandName.equals("dev")) {
+                DevCommand.triggerDevCommand(interaction, api);
+            } else if (fullCommandName.equals("change")) {
+                ChangeBatteries.change(api, interactionServer);
+            } else if (fullCommandName.equals("randomprompt")){
+                RandomPrompt.randomPrompt(interaction);
+            } else if (fullCommandName.equals("reklama")) {
+                ReklamaCommand.reklama(api, interaction);
+            } else if (fullCommandName.equals("addgreeting")) {
+                AddGreetingCommand.greetingCommand(interaction);
+            } else if (fullCommandName.equals("greetingdel")) {
+                RemoveGreetingCommand.removeGreeting(interaction);
+            } else if (fullCommandName.equals("greetingls")) {
+                GreetingListCommand.greetingList(interaction);
             }
         });
     }
